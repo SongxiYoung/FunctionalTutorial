f137b1ea591acf1887335a58ea3f5363f656725b
SongxiYoung <songxi.young@gmail.com>
2024-01-27 10:28:19 -0600

    04-02

--- for_students/F-04-02.js
+++ for_students/F-04-02.js
@@ -1,2 +1,21 @@
 // left empty for the student to fill in
 // the buttons have ids button1, button2, ...
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+const button5 = document.getElementById("button5");
+
+function buttonHandler(bottonName) {
+    let count = 0;
+    bottonName.onclick = function() { 
+        count += 1;
+        bottonName.innerHTML = count;
+        }
+}
+
+buttonHandler(button1);
+buttonHandler(button2);
+buttonHandler(button3);
+buttonHandler(button4);
+buttonHandler(button5);
